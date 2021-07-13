321f15fc5e270980b91c19a54599cf8541f6105c
hazlena <hzlena@gmail.com>
2021-07-13 12:15:25 +0800

    yang betul

--- App.js
+++ App.js
@@ -4,18 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hai.. Nama saya Hazlena!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1,}}>
+    <View style={{ flex: 1,}}>
+     <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "yellow" }}></View>
+      <View style={{ flex: 1, backgroundColor: "magenta" }}></View>
+      <View style={{ flex: 1, backgroundColor: "cyan" }}></View>
     </View>
-  );
-}
+    </View>
+  
+    <View style={{ flex: 5,backgroundColor: "green" }}>
+    </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+     
+    <View style={{ flex: 1,}}>
+     <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "black" }}></View>
+      <View style={{ flex: 1, backgroundColor: "cyan" }}></View>
+    </View>
+    </View>
+ 
+    </View>);
+}
